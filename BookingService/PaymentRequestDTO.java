eab6ec017b61e6c17c7bfbb2340f45f2f6288818
afsahareeb <afsahareeb7862@gmail.com>
2025-03-03 23:17:54 +0500

    Extra folder deleted

--- BookingService/PaymentRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookingservice.dto;
-
-import lombok.Data;
-
-@Data
-public class PaymentRequestDTO {
-    private String userId;
-    private double amount;  // Total amount to be paid for the booking
-}
